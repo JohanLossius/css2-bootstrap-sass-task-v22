47d375175cf57fbfca408f14605aa5b5ec6da859
JohanLossius <52312393+JohanLossius@users.noreply.github.com>
2023-04-18 19:16:26 +0200

    Initial

---
+++ js/script.js
@@ -0,0 +1,3 @@
+
+// "build": "sass node_modules/scss/custom.scss:node_modules/bootstrap/dist/css/custom_bootstrap.css",
+// "watch": "sass --watch node_modules/scss/custom.scss:node_modules/bootstrap/dist/css/custom_bootstrap.css & live-server"
